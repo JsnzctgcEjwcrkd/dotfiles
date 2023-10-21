6344ac37ed746ec3802d7726910dd6feb98fdfb5
boku <84499406+JsnzctgcEjwcrkd@users.noreply.github.com>
2023-10-21 23:08:22 +0900

    add install script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dotfiles
+
+## Target environment
+
+ubuntu jammy
+
+## Install
+
+```bash
+git clone https://github.com/JsnzctgcEjwcrkd/dotfiles.git
+cd dotfiles
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
-cd `dirname $0`
-SCRIPT_PATH=$(dirname $0)
+cd "$(dirname "$0")" || exit
+SCRIPT_PATH=$(pwd)
 
 sudo apt-get update
 sudo apt-get -y upgrade
 
 # create link
-ln -s $SCRIPT_PATH/.zshrc ~
-ln -s $SCRIPT_PATH/.p10k.zsh ~
+ln -s "$SCRIPT_PATH"/.zshrc ~
+ln -s "$SCRIPT_PATH"/.p10k.zsh ~
 
 # zsh
 sudo apt-get -y install zsh
 chsh -s /bin/zsh
-sudo su $USER
+sudo su "$USER"
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # p10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
